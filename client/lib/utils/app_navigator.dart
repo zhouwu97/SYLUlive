7f2c52836019be482eb2ee0461ce609b3541e6a4
kangwu zhou <3170305904@qq.com>
2026-05-22 09:27:27 +0800

    feat: Home widget service and navigation
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- client/lib/utils/app_navigator.dart
+++ client/lib/utils/app_navigator.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 /// 全局导航 Key，用于在 Dio 拦截器等无 BuildContext 的地方执行导航
 final GlobalKey<NavigatorState> appNavigatorKey = GlobalKey<NavigatorState>();
+
+/// 桌面小组件点击 → 通知 HomeScreen 切到课程表 tab
+final ValueNotifier<int> widgetTabSwitch = ValueNotifier<int>(0);

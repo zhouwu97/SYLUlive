30d773224b823b0ae2ce4cc7eb72c834ef459d6d
kangwu zhou <3170305904@qq.com>
2026-06-16 23:20:39 +0800

    Bust cached Flutter web entrypoint

--- client/web/flutter_bootstrap.js
+++ client/web/flutter_bootstrap.js
@@ -1,5 +1,14 @@
 {{flutter_js}}
 {{flutter_build_config}}
+
+const mainJsVersion = '20260616-2307';
+const dart2jsBuild = _flutter.buildConfig.builds.find(
+  (build) => build.compileTarget === 'dart2js',
+);
+if (dart2jsBuild && dart2jsBuild.mainJsPath === 'main.dart.js') {
+  dart2jsBuild.mainJsPath = `main.dart.js?v=${mainJsVersion}`;
+}
+
 _flutter.loader.load({
   config: {
     canvasKitBaseUrl: "canvaskit/",

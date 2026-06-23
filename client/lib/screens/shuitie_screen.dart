f0430cd8c9df99b241a1c0afd7ccf1e30efae4f6
kangwu zhou <3170305904@qq.com>
2026-06-23 11:19:24 +0800

    fix: add 404 fallback for announcements API to support older backends

--- client/lib/screens/shuitie_screen.dart
+++ client/lib/screens/shuitie_screen.dart
@@ -190,7 +190,17 @@ class _ShuitieScreenState extends State<ShuitieScreen>
   Future<void> _loadAnnouncements() async {
     final authProvider = context.read<AuthProvider>();
     try {
-      final response = await authProvider.dio.get(ApiConstants.noticesPath);
+      var response;
+      try {
+        response = await authProvider.dio.get(ApiConstants.noticesPath);
+      } on DioException catch (e) {
+        if (e.response?.statusCode == 404) {
+          response = await authProvider.dio.get('/announcements');
+        } else {
+          rethrow;
+        }
+      }
+
       if (response.statusCode == 200) {
         final all = (response.data as List)
             .map((e) => model.Announcement.fromJson(e))
